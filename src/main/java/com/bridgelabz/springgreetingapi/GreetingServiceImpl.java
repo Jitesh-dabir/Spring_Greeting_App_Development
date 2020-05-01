e467abd9fa170d0b2605a20837d5b594c8da9f6b
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-01 18:07:57 +0530

    Modified: Method return default message if first and last name is null

--- src/main/java/com/bridgelabz/springgreetingapi/GreetingServiceImpl.java
+++ src/main/java/com/bridgelabz/springgreetingapi/GreetingServiceImpl.java
@@ -8,6 +8,8 @@ public class GreetingServiceImpl implements IGreetingService {
     private static final String template = "Hello world!";
 
     public String greet(String firstName, String lastName) {
+        if (firstName == null && lastName == null)
+            return template;
         return template.replace("world", firstName + " " + lastName);
 
     }
