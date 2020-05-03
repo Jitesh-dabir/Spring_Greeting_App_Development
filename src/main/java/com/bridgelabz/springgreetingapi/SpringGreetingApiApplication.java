9799b9db390c08cdb65d92192ccd5a754ca64352
Jitesh-Dabir <jitudabir9@gmail.com>
2020-05-03 17:28:58 +0530

    Dependancy for all resources

---
+++ src/main/java/com/bridgelabz/springgreetingapi/SpringGreetingApiApplication.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.springgreetingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGreetingApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGreetingApiApplication.class, args);
+    }
+
+}
